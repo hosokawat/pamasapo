8d08907c00b35d376190baad561ffc2b7b28b556
seitaro <seitaro.hrkw@gmail.com>
2019-07-15 20:31:02 +0900

    保育園の情報を表示するためのコンポーネントを分割
    レイアウトを調整。
    表示する情報はひとまず、全てを表示するようにした。

---
+++ components/nurseries/NursriesDetail.vue
@@ -0,0 +1,459 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h2>{{ item.name }}</h2>
+        <p>({{ item.kana }})</p>
+        <v-chip v-if="item.nursery.facility.nurseryOwner">{{ item.nursery.facility.nurseryOwner }}</v-chip>
+        <v-chip>{{ item.nursery.facility.nurseryType }}</v-chip>
+        <v-chip v-if="item.nursery.facility.nurserySubType">{{ item.nursery.facility.nurserySubType }}</v-chip>
+        <v-chip>{{ item.nursery.facility.ownership }}</v-chip>
+        <v-list dense>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>住所</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ address }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>空き情報</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{availability_summary}}({{item.nursery.status.baseDate}})</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>時間</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>
+              {{ period(item.nursery.facility.openingTime,item.nursery.facility.closingTime) }}
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.phone">
+            <v-flex xs3>
+              <v-list-tile-title>電話番号</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.phone }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.fax">
+            <v-flex xs3>
+              <v-list-tile-title>FAX番号</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.fax }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.email">
+            <v-flex xs3>
+              <v-list-tile-title>メールアドレス</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.email }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.website">
+            <v-flex xs3>
+              <v-list-tile-title>ウェブサイト</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.website }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.opened_on">
+            <v-flex xs3>
+              <v-list-tile-title>建築年月日</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.opened_on }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.rebuild_on">
+            <v-flex xs3>
+              <v-list-tile-title>改築年月日</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.rebuild_on }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.remarks_basic">
+            <v-flex xs3>
+              <v-list-tile-title>施設基本情報備考</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.remarks_basic }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile v-if="item.nursery.facility.hasParkingLot">
+            <v-flex xs3>
+              <v-list-tile-title>駐車場台数</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.numberOfParkingLot }}台</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-btn block>お気に入り</v-btn>
+          </v-list-tile>
+        </v-list>
+        <ShowMap
+          :access-token="accessToken"
+          :lng="item.long"
+          :lat="item.lat"
+        />
+
+        <v-subheader>施設</v-subheader>
+        <v-list dense>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>時間</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ period(item.nursery.facility.openingTime,item.nursery.facility.closingTime) }}
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>保育短時間</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{
+              period(item.nursery.facility.standard_opening_time,item.nursery.facility.standard_closing_time) }}
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>保育短時間</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{
+              period(item.nursery.facility.short_opening_time,item.nursery.facility.short_closing_time) }}
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>保育年齢</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ facility_age }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>定員(1号)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.capacity_1 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>定員(2号)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.capacity_2 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>定員(3号)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.capacity_3 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <!--          -->
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>保育室の広さ</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.area_of_nursery_room }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile v-if="item.nursery.facility.has_yard">
+            <v-flex xs3>
+              <v-list-tile-title>園庭の広さ</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.area_of_yard }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>プール</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.hasPool | ari_nashi }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile v-if="item.nursery.facility.hasParkingLot">
+            <v-flex xs3>
+              <v-list-tile-title>駐車場台数</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.numberOfParkingLot }}台</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>備考</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.facility.remarks_facility }}</v-list-tile-content>
+          </v-list-tile>
+
+        </v-list>
+        <v-subheader>サービス</v-subheader>
+        <v-list dense>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>産休明け保育</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.supportMaturnityLeave | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>土曜保育</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.saturdayCareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>休日保育</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.holidayCareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>一時預かり(定期)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.temporaryCareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>一時預かり(不定期)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.spotCareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>延長保育</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.extendedCareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>夜間保育</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.nightCareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>24時間保育</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.h24CareService | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>給食</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.provideLunch | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.service.provideLunch">
+            <v-flex xs3>
+              <v-list-tile-title>主食(3歳以上)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.lunchWithStaple1 }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="item.nursery.service.provideLunch">
+            <v-flex xs3>
+              <v-list-tile-title>主食(3歳未満)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.lunchWithStaple2 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>おやつ</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.provideSnacks | maru_batsu}}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>送迎バス</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.provideShuttleBus | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>制服</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.useUniform | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>スモック</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.useSmock | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>体操服</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.useTrainingWear | maru_batsu }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>備考</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.service.remarksService }}</v-list-tile-content>
+          </v-list-tile>
+
+        </v-list>
+
+        <v-subheader>入所状況</v-subheader>
+        <v-list dense>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所児童数(0歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.numbers0 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所児童数(1歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.numbers1 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所児童数(2歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.numbers2 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所児童数(3歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.numbers3 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所児童数(4歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.numbers4 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所児童数(5歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.numbers5 }}</v-list-tile-content>
+          </v-list-tile>
+
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所待ち児童数(0歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.standbys0 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所待ち児童数(1歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.standbys1 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所待ち児童数(2歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.standbys2 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所待ち児童数(3歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.standbys3 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所待ち児童数(4歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.standbys4 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所待ち児童数(5歳)</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.standbys5 }}</v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title>入所状況基準日</v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>{{ item.nursery.status.baseDate }}</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  import ShowMap from '~/components/nurseries/ShowMap'
+
+  export default {
+    name: 'NursriesDetail',
+    components: {ShowMap},
+    props: {
+      item: {},
+      accessToken: {type: String, required: true},
+    },
+    methods: {
+      period: function (startTime, endTime) {
+        if (startTime === undefined || endTime === undefined) {
+          return ''
+        }
+        return `${startTime} 〜 ${endTime}`
+      },
+    },
+    filters: {
+      ari_nashi: function (value) {
+        if (value) {
+          return 'あり'
+        } else {
+          return 'なし'
+        }
+      },
+      maru_batsu: function (value) {
+        if (value) {
+          return '○'
+        } else {
+          return '✕'
+        }
+      },
+    },
+    computed: {
+      address: function () {
+        const item = this.item
+        return `${item.prefecture}${item.city}${item.ward}${item.address}`
+      },
+      facility_age: function () {
+        const facility = this.item.nursery.facility
+        if (facility.age_from === undefined || facility.age_to === undefined) {
+          return ''
+        }
+        return `${facility.age_from}歳から${facility.age_to}歳`
+      },
+      availability_summary: function () {
+        const status = this.item.nursery.status
+        const keys = [
+          'availability_0',
+          'availability_1',
+          'availability_2',
+          'availability_3',
+          'availability_4',
+          'availability_5'
+        ]
+        // TODO: 空き情報をサマライズするためのルールを決める
+        return 'あり/なし'
+      }
+    },
+  }
+</script>
--- pages/nurseries/_id.vue
+++ pages/nurseries/_id.vue
@@ -1,106 +1,17 @@
 <template>
   <div>
-    <v-container v-if="item">
-      <pre>{{item}}</pre>
-      <h2>{{ item.name }}</h2>
-      <v-chip>{{ item.nursery.facility.nurseryType }}</v-chip>
-      <v-chip>{{ item.nursery.facility.ownership }}</v-chip>
-      <v-list>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>住所</v-list-tile-title>
-            <v-list-tile-sub-title>{{ address }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>欠員</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>時間</v-list-tile-title>
-            <v-list-tile-sub-title>{{ period }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile v-if="item.nursery.facility.hasParkingLot">
-          <v-list-tile-content>
-            <v-list-tile-title>駐車場台数</v-list-tile-title>
-            <v-list-tile-sub-title>{{ item.nursery.facility.numberOfParkingLot }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-btn block>お気に入り</v-btn>
-        </v-list-tile>
-      </v-list>
-      <ShowMap
-        :access-token="accessToken"
-        :lng="item.long"
-        :lat="item.lat"
-      />
-      <v-list>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>延長保育</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>一時保育</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>夜間保育</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>24時間</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>年齢</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>定員</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>スモック</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-title>申込み倍率</v-list-tile-title>
-            <v-list-tile-sub-title></v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-container>
+    <NursriesDetail v-if="item" :item="item" :accessToken="accessToken" />
     <p v-else>保育園のデータが取得できませんでした。</p>
   </div>
 </template>
 
 <script>
-  import ShowMap from '~/components/nurseries/ShowMap'
   import * as queries from '~/graphql/queries/get'
+  import NursriesDetail from "~/components/nurseries/NursriesDetail";
 
   export default {
     name: "nurseries_id",
-    components: {ShowMap},
+    components: {NursriesDetail},
     async asyncData({app, params, env}) {
       const client = app.$apiClient
 
@@ -118,19 +29,7 @@
         item: get_data
       }
     },
-    computed: {
-      address: function () {
-        const item = this.item
-        return `${item.prefecture}${item.city}${item.ward}${item.address}`
-      },
-      period: function () {
-        const item = this.item
-        return `${item.nursery.facility.openingTime} 〜 ${item.nursery.facility.closingTime}`
-      }
-    },
   }
 
 </script>
 
-<style scoped>
-</style>
