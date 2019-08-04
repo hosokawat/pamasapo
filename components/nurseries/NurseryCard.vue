4d480f933564c2012ddeb24b9eaaec3de56512d5
seitaro <seitaro.hrkw@gmail.com>
2019-08-04 17:39:46 +0900

    保育園のデータを表示するためのCardをコンポーネントに分ける

---
+++ components/nurseries/NurseryCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-text class="px-0">
+      <span>保育所かな　{{ item.kana }}</span><br>
+      <span class='headline'>保育所名　{{ item.name }}</span><br>
+      <span>住所　{{ item.postalCode + item.prefecture + item.city + item.ward + item.address }}</span><br>
+      <span>空き状況({{ item.base_date }})</span><br>
+      <span>開園時間　{{ item.openingTime }}から{{ item.openingTime }}まで</span><br>
+      <span v-if='item.number_of_parking_lot>0'>駐車場 あり{{ item.number_of_parking_lot }}台</span>
+      <span v-else>駐車場 なし</span>
+    </v-card-text>
+    <img alt="写真" src='~assets/image.png'>
+    <v-card-actions>
+      <FavoriteButton :id="item.id" />
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+  import FavoriteButton from "~/components/FavoriteButton"
+
+  export default {
+    name: 'NurseryCard',
+    components: {FavoriteButton},
+    props: {
+      item: {}
+    }
+  }
+</script>
--- pages/nurseries/index.vue
+++ pages/nurseries/index.vue
@@ -19,7 +19,8 @@
           box
         ></v-select>
       </v-flex>
-
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs6>
         <v-btn block color="grey lighten-3">常時</v-btn>
       </v-flex>
@@ -27,40 +28,25 @@
       <v-flex xs6>
         <v-btn block color="grey lighten-3">新年度</v-btn>
       </v-flex>
-
-      <template v-for="item in centers">
-        <v-flex xs12 :key="item.name">
-          <v-card>
-            <v-card-text class="px-0">
-              <span>保育所かな　{{ item.kana }}</span><br>
-              <span class='headline'>保育所名　{{ item.name }}</span><br>
-              <span>住所　{{ item.postalCode + item.prefecture + item.city + item.ward + item.address }}</span><br>
-              <span>空き状況({{ item.base_date }})</span><br>
-              <span>開園時間　{{ item.openingTime }}から{{ item.openingTime }}まで</span><br>
-              <span v-if='item.number_of_parking_lot>0'>駐車場 あり{{ item.number_of_parking_lot }}台</span>
-              <span v-else>駐車場 なし</span>
-            </v-card-text>
-            <img alt="写真" src='./image.png'>
-            <v-card-actions>
-              <v-btn block color="amber" @click="toggleFavorites(item.id)">
-                <v-icon>favorite</v-icon>
-                お気に入り{{ favoriteList.includes(item.id) ? 'から削除'　: 'に追加' }}
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </template>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 v-for="item in centers" :key="item.name">
+        <NurseryCard :item="item" />
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
   import {mapGetters} from 'vuex'
+  import NurseryCard from "~/components/nurseries/NurseryCard.vue"
 
   export default {
+    components: {NurseryCard},
     data() {
       return {
-        filter_items: ['許認可',
+        filter_items: [
+          '許認可',
           '私立・公立',
           '開園時間',
           '閉園時間',
@@ -68,8 +54,10 @@
           '延長',
           '一時保育',
           '夜間・休日',
-          '駐車場'],
-        sort_items: ['許認可',
+          '駐車場'
+        ],
+        sort_items: [
+          '許認可',
           '私立・公立',
           '開園時間',
           '閉園時間',
@@ -77,8 +65,8 @@
           '延長',
           '一時保育',
           '夜間・休日',
-          '駐車場'],
-        favoriteList: []
+          '駐車場'
+        ],
       }
     },
 
@@ -91,23 +79,5 @@
     async fetch({store}) {
       await store.dispatch('center/search')
     },
-
-    methods: {
-      toggleFavorites: function (id) {
-        if (this.favoriteList.includes(id)) {
-          this.favoriteList = this.favoriteList.filter(el => el != id)
-        } else {
-          this.favoriteList.push(id)
-        }
-        localStorage.setItem('favorite', JSON.stringify(this.favoriteList))
-      }
-    },
-
-    created: function () {
-      if (localStorage.getItem('favorite')) {
-        this.favoriteList = JSON.parse(localStorage.getItem('favorite'))
-      }
-    },
-
   }
 </script>
