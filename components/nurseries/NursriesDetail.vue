2bf5f2ab752e3d183c51785a83d6d6bf11707029
seitaro <seitaro.hrkw@gmail.com>
2019-08-04 15:29:53 +0900

    空きのありなしをサマライズして表示する

--- components/nurseries/NursriesDetail.vue
+++ components/nurseries/NursriesDetail.vue
@@ -21,6 +21,42 @@
             </v-flex>
             <v-list-tile-content>{{availability_summary}}({{item.nursery.status.baseDate}})</v-list-tile-content>
           </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title></v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>0歳 {{this.item.nursery.status.availability_0}}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title></v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>1歳 {{this.item.nursery.status.availability_1}}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title></v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>2歳 {{this.item.nursery.status.availability_2}}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title></v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>3歳 {{this.item.nursery.status.availability_3}}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title></v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>4歳 {{this.item.nursery.status.availability_4}}</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-flex xs3>
+              <v-list-tile-title></v-list-tile-title>
+            </v-flex>
+            <v-list-tile-content>5歳 {{this.item.nursery.status.availability_5}}</v-list-tile-content>
+          </v-list-tile>
 
           <v-list-tile>
             <v-flex xs3>
@@ -444,16 +480,19 @@
       },
       availability_summary: function () {
         const status = this.item.nursery.status
-        const keys = [
-          'availability_0',
-          'availability_1',
-          'availability_2',
-          'availability_3',
-          'availability_4',
-          'availability_5'
+        const availabilities = [
+          status['availability_0'],
+          status['availability_1'],
+          status['availability_2'],
+          status['availability_3'],
+          status['availability_4'],
+          status['availability_5']
         ]
-        // TODO: 空き情報をサマライズするためのルールを決める
-        return 'あり/なし'
+        // すべてのitemsが✕でなければ空きあり
+        const is_available = !(availabilities.every((item) => {
+          return item === '✕'
+        }))
+        return (is_available) ? 'あり' : 'なし'
       }
     },
   }
