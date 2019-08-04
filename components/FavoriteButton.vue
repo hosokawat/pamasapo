aa12951173034410bd18d96ff13e4dcef1d9e3a7
seitaro <seitaro.hrkw@gmail.com>
2019-08-04 16:37:36 +0900

    FavoriteButton.vueで不要なdivタグを除去

--- components/FavoriteButton.vue
+++ components/FavoriteButton.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <v-btn block color="amber" v-if="can_favorited()" @click="add_favorite">
-      <v-icon>favorite</v-icon>
-      お気に入りに追加
-    </v-btn>
-    <v-btn block color="" v-else @click="remove_favorite">
-      <v-icon>favorite</v-icon>
-      お気に入りから削除
-    </v-btn>
-  </div>
+  <v-btn block color="amber" v-if="can_favorited()" @click="add_favorite">
+    <v-icon>favorite</v-icon>
+    お気に入りに追加
+  </v-btn>
+  <v-btn block color="" v-else @click="remove_favorite">
+    <v-icon>favorite</v-icon>
+    お気に入りから削除
+  </v-btn>
 </template>
 <script>
   export default {
