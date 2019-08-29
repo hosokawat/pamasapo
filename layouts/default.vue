3d40eb2aa3476c5f5832d96c210ae4fe3707847a
hosokawa.takahiro <vwxyzzy@gmail.com>
2019-08-29 20:41:47 +0900

    進捗コミット

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,16 +16,18 @@
         <span>お気に入り</span>
         <v-icon>favorite</v-icon>
       </v-btn>
+      <v-btn to="/recent/">
+        <span>履歴</span>
+        <v-icon>history</v-icon>
+      </v-btn>
     </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-
-      }
-    }
+export default {
+  data() {
+    return {};
   }
+};
 </script>
--- pages/recent/index.vue
+++ pages/recent/index.vue
@@ -1,15 +1,76 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5">
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;最近みた場所</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12 v-for="item in centers" :key="item.name">
+        <NurseryCard :item="item" />
+        <p class="grey--text lighten-4 float-right">2019/7/7 12時00分に閲覧しました</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import NurseryCard from "~/components/nurseries/NurseryCard.vue";
+
+export default {
+  components: { NurseryCard },
+  data() {
+    return {};
+  },
+
+  computed: {
+    ...mapGetters({
+      centers: "center/items"
+    })
+  },
+
+  async fetch({ store }) {
+    await store.dispatch("center/search");
+  },
+  methods: {
+    history: function() {}
+  }
+};
+/*
+  components: { NurseryCard } //,
+  /*
+data() {
+      return {
+        filter_items: [
+          '許認可',
+          '私立・公立',
+          '開園時間',
+          '閉園時間',
+          '24時間',
+          '延長',
+          '一時保育',
+          '夜間・休日',
+          '駐車場'
+        ],
+        sort_items: [
+          '許認可',
+          '私立・公立',
+          '開園時間',
+          '閉園時間',
+          '24時間',
+          '延長',
+          '一時保育',
+          '夜間・休日',
+          '駐車場'
+        ],
+      }
+    },
+
+    computed: {
+      ...mapGetters({
+        centers: 'center/items'
+      })
+    },
+
+    async fetch({store}) {
+      await store.dispatch('center/search')
+    },
+};
+*/
+</script>
